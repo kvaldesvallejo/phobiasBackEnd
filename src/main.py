f2daeb713cd70929b85dbc2b2281d17152884e0b
kvaldesvallejo <kvaldesvallejo@gmail.com>
2021-03-01 00:21:33 +0000

    Table user with get and insert endpoint

--- src/main.py
+++ src/main.py
@@ -9,7 +9,16 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
-#from models import Person
+
+#import cloudinary
+#import cloudinary.uploader
+#import cloudinary.api
+
+#cloudinary.config( 
+ # cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME'), 
+  #api_key = os.environ.get('CLOUDINARY_API_KEY'), 
+  #api_secret = os.environ.get('CLOUDINARY_API_SECRET') 
+#)
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -38,6 +47,102 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# Create a new user 
+@app.route('/user', methods=['POST'])
+def create_user():
+    body = request.get_json()
+    #print(  request)
+    #print(  body)
+    
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", 400)
+
+    # Validations
+    #First Name
+    if "fisrt_name" in body:
+        #print ("fisrt_name[" + str(body['fisrt_name']) +"]")
+        fn = body['fisrt_name']
+    else:
+        raise APIException("You need to specify the first name", 400)
+    #Last name
+    if "last_name" in body:
+        ln = body['last_name']
+        #user["last_name"] = body["last_name"]
+    else:
+        raise APIException("You need to specify an Last Name", 400)
+    #email
+    if "email" in body:
+        e= body["email"]
+    else:
+        raise APIException("You need to specify an email", 400)
+    if User.query.filter_by(email=body['email']).first() is not None:
+        raise APIException("email is in use", 400)
+
+    #phone_number
+    if "phone_number" in body:
+        phn = body["phone_number"]
+    else:
+        raise APIException("You need to specify an phone_number", 400)
+    if User.query.filter_by(phone_number=body['phone_number']).first() is not None:
+        raise APIException("phone_number is in use", 400)
+
+    #user_name
+    if "user_name" in body:
+        un = body["user_name"]
+    else:
+        raise APIException("You need to specify an user_name", 400)
+    if User.query.filter_by(user_name=body['user_name']).first() is not None:
+        raise APIException("user name is in use", 400)
+
+    #password
+    if "password" in body:
+        p = body["password"]
+    else:
+        raise APIException("You need to specify an password", 400)
+    if User.query.filter_by(password=body['password']).first() is not None:
+        raise APIException("password is in use", 400)
+
+    #profile_picture
+    if "profile_picture" in body:
+        pp = body["profile_picture"]
+    else:
+        pp = "default"
+
+    #account_type
+    if "account_type" in body:
+        at = body["account_type"]
+    else:
+        raise APIException("You need to specify the account_type", 400)
+
+    new_user = User(fisrt_name = fn, 
+                    last_name = ln, 
+                    email= e, 
+                    phone_number = phn, 
+                    user_name = un, 
+                    password = p, 
+                    profile_picture = pp, 
+                    account_type = at)
+
+    print(new_user)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    users = User.query.all()
+    response_body = list(map(lambda x: x.serialize(), users))
+    #print(response_body)
+    #print(users)
+    return jsonify(response_body), 200
+
+@app.route('/users', methods=['GET'])
+def get_users():
+
+    users = User.query.all()
+    response_body = list(map(lambda x: x.serialize(), users))
+    print(response_body)
+    print(users)
+    return jsonify(response_body), 200    
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,16 +4,27 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    fisrt_name = db.Column(db.String(10), unique=False, nullable=False)
+    last_name = db.Column(db.String(20), unique=False, nullable=False)
+    phone_number = db.Column(db.String(15), unique=True, nullable=False)
+    user_name = db.Column(db.String(20), unique=True, nullable=False)
+    profile_picture = db.Column(db.String(120), nullable=True)
+    account_type = db.Column(db.Integer, unique=False, nullable=False) 
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.user_name
 
     def serialize(self):
         return {
             "id": self.id,
+            "fisrt_name" : self.fisrt_name,
+            "last_name" : self.last_name,
+            "phone_number" : self.phone_number,
+            "user_name" : self.user_name,
+            "profile_picture" : self.profile_picture,
+            "account_type" : self.account_type, 
             "email": self.email,
             # do not serialize the password, its a security breach
         }
