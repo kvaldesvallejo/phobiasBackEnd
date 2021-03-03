31424148eeac84f9cf98cc3e508916a7c8050adc
kvaldesvallejo <kvaldesvallejo@gmail.com>
2021-03-03 23:19:58 +0000

    User, Patient and Therapist

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Therapist , Patient
 
 #import cloudinary
 #import cloudinary.uploader
@@ -48,12 +48,11 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+#-----------------USER-----------------------------------------------------------
 # Create a new user 
 @app.route('/user', methods=['POST'])
 def create_user():
     body = request.get_json()
-    #print(  request)
-    #print(  body)
     
     if body is None:
         raise APIException("You need to specify the request body as a json object", 400)
@@ -61,14 +60,12 @@ def create_user():
     # Validations
     #First Name
     if "fisrt_name" in body:
-        #print ("fisrt_name[" + str(body['fisrt_name']) +"]")
         fn = body['fisrt_name']
     else:
         raise APIException("You need to specify the first name", 400)
     #Last name
     if "last_name" in body:
         ln = body['last_name']
-        #user["last_name"] = body["last_name"]
     else:
         raise APIException("You need to specify an Last Name", 400)
     #email
@@ -131,10 +128,9 @@ def create_user():
 
     users = User.query.all()
     response_body = list(map(lambda x: x.serialize(), users))
-    #print(response_body)
-    #print(users)
     return jsonify(response_body), 200
 
+# Get all users
 @app.route('/users', methods=['GET'])
 def get_users():
 
@@ -143,6 +139,265 @@ def get_users():
     print(response_body)
     print(users)
     return jsonify(response_body), 200    
+
+# Get an specific user by id 
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_user_by_id(user_id):
+    
+    target_user = User.query.get(user_id)
+    if target_user is None:
+        raise APIException("User not found", 400)
+
+    return jsonify(target_user.serialize(), 200)
+
+# Update an specific user by id 
+@app.route('/user/<int:user_id>', methods=['POST'])
+def handle_single_user(user_id):
+
+    target_user = User.query.get(user_id)
+    if target_user is None:
+        raise APIException("User not found", 400)
+
+    body = request.get_json()
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", 400)
+
+    # Validations
+    #First Name
+    if "fisrt_name" in body:
+        target_user.fisrt_name = body['fisrt_name']
+    
+    #Last name
+    if "last_name" in body:
+        target_user.last_name = body['last_name']
+    
+    #email
+    if "email" in body:
+        if User.query.filter_by(email=body['email']).first() is not None:
+            raise APIException("Email is in use", 400)
+        target_user.email = body["email"]
+
+    #phone_number
+    if "phone_number" in body:
+        if User.query.filter_by(phone_number=body['phone_number']).first() is not None:
+            raise APIException("phone_number is in use", 400)
+        target_user.phone_number = body["phone_number"]
+
+    #user_name
+    if "user_name" in body:
+        if User.query.filter_by(user_name=body['user_name']).first() is not None:
+            raise APIException("user name is in use", 400)
+        target_user.user_name = body["user_name"]
+    
+    #password
+    if "password" in body:
+        target_user.password = body["password"]
+    
+    #profile_picture
+    if "profile_picture" in body:
+        target_user.profile_picture = body["profile_picture"]
+
+    #account_type
+    if "account_type" in body:
+        target_user.account_type = body["account_type"]
+                        
+    db.session.commit()
+            
+    users = User.query.all()
+    response_body = list(map(lambda x: x.serialize(), users))
+    return jsonify(response_body), 200
+
+#-----------------Therapist-----------------------------------------------------------
+# Create a new Therapist 
+@app.route('/therapist', methods=['POST'])
+def create_therapist():
+    body = request.get_json()
+    
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", 400)
+
+    # Validations
+    #user_id
+    if "user_id" in body:
+        ui = body["user_id"]
+    else:
+        raise APIException("You need to specify an user_id", 400)
+    #ofice_location
+    if "ofice_location" in body:
+        ol = body["ofice_location"]
+    else:
+        raise APIException("You need to specify an ofice_location", 400)
+    #experience
+    if "experience" in body:
+        e = body["experience"]
+    else:
+        e = "default"
+    #languages_spoke
+    if "languages_spoke" in body:
+        ls = body["languages_spoke"]
+    else:
+        raise APIException("You need to specify the languages_spoke", 400)
+
+    new_therapist = Therapist(user_id = ui, 
+                    ofice_location = ol, 
+                    experience = e, 
+                    languages_spoke = ls)
+
+    print(new_therapist)
+
+    db.session.add(new_therapist)
+    db.session.commit()
+
+    therapists = Therapist.query.all()
+    response_body = list(map(lambda x: x.serialize(), therapists))
+    return jsonify(response_body), 200
+       
+# Get all Therapist
+@app.route('/therapists', methods=['GET'])
+def get_therapists():
+    therapists = Therapist.query.all()
+    response_body = list(map(lambda x: x.serialize(), therapists))
+    return jsonify(response_body), 200
+
+# Get an specific Therapist by id 
+@app.route('/therapist/<int:user_id>', methods=['GET'])
+def get_therapist(user_id):
+    target_therapists = Therapist.query.get(user_id)
+    if target_therapists is None:
+        raise APIException("User not found", 400)
+
+    return jsonify(target_therapists.serialize(), 200)
+
+# Update an specific Therapist by id
+@app.route('/therapist<int:user_id>', methods=['POST'])
+def update_therapist(user_id):
+
+    target_therapist = User.query.get(user_id)
+
+    if target_therapist is None:
+        raise APIException("Therapist not found", 400)
+
+    body = request.get_json()
+    
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", 400)
+
+    # Validations
+    #user_id
+    if "user_id" in body:
+        target_therapist.user_id = body['user_id']
+    
+    #ofice_location
+    if "ofice_location" in body:
+       target_therapist.ofice_location = body['ofice_location']
+    
+    #experience
+    if "experience" in body:
+        target_therapist.experience = body['experience']
+   
+    #languages_spoke
+    if "languages_spoke" in body:
+        target_therapist.languages_spoke = body['languages_spoke']
+
+    db.session.commit()
+
+    therapists = Therapist.query.all()
+    response_body = list(map(lambda x: x.serialize(), therapists))
+    return jsonify(response_body), 200
+
+#-----------------Patient-----------------------------------------------------------
+# Create a new Patient 
+@app.route('/patient', methods=['POST'])
+def create_patient():
+
+    body = request.get_json()
+
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", 400)
+
+    # Validations
+    #user_id
+    if "user_id" in body:
+        ui = body["user_id"]
+    else:
+        raise APIException("You need to specify an user_id", 400)
+    #wishfearLESS
+    if "wishfearLESS" in body:
+        w = body["wishfearLESS"]
+    #previous_help
+    if "previous_help" in body:
+        ph = body["previous_help"]
+    #zc
+    if "zc" in body:
+        zipc = body["zc"]
+    else:
+        raise APIException("You need to specify the languages_spoke", 400)
+
+    new_patient = Patient(user_id = ui, 
+                    wishfearLESS = w, 
+                    previous_help = ph, 
+                    zc = zipc)
+
+    db.session.add(new_patient)
+    db.session.commit()
+
+    patients = Patient.query.all()
+    response_body = list(map(lambda x: x.serialize(), patients))
+    return jsonify(response_body), 200
+
+# Get all Patient
+@app.route('/patients', methods=['GET'])
+def get_patients():
+    patients = Patient.query.all()
+    response_body = list(map(lambda x: x.serialize(), patients))
+    return jsonify(response_body), 200
+
+# Get an specific Patient by id 
+@app.route('/patient/<int:user_id>', methods=['GET'])
+def get_patient_by_id(user_id):
+    target_patient = Patient.query.get(user_id)
+    if target_patient is None:
+        raise APIException("User not found", 400)
+
+    return jsonify(target_patient.serialize(), 200)
+
+# Update an specific Patient by id
+@app.route('/patient<int:user_id>', methods=['POST'])
+def update_patient(user_id):
+
+    target_patient = Patient.query.get(user_id)
+
+    if target_patient is None:
+        raise APIException("Patient not found", 400)
+
+    body = request.get_json()
+    
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", 400)
+
+    # Validations
+    #user_id
+    if "user_id" in body:
+        target_patient.user_id = body['user_id']
+    
+    #wishfearLESS
+    if "wishfearLESS" in body:
+       target_patient.wishfearLESS = body['wishfearLESS']
+    
+    #previous_help
+    if "previous_help" in body:
+        target_patient.previous_help = body['previous_help']
+   
+    #zc
+    if "zc" in body:
+        target_patient.zc = body['zc']
+
+    db.session.commit()
+
+    patients = Patient.query.all()
+    response_body = list(map(lambda x: x.serialize(), patients))
+    return jsonify(response_body), 200
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -28,3 +28,40 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Therapist(db.Model):
+    #int FK >- User.id
+    user_id = db.Column(db.Integer, primary_key=True)
+    ofice_location = db.Column(db.String(30), unique=False, nullable=False)
+    experience = db.Column(db.String(500), unique=False, nullable=False)
+    languages_spoke = db.Column(db.String(300), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Therapist %r>' % self.user_id
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "ofice_location" : self.ofice_location,
+            "experience" : self.experience,
+            "languages_spoke" : self.languages_spoke,
+        }
+
+class Patient(db.Model):
+    #int FK >- User.id
+    user_id = db.Column(db.Integer, primary_key=True)
+    wishfearLESS = db.Column(db.String(300), unique=False, nullable=True)
+    previous_help = db.Column(db.Boolean, unique=False, nullable=True)
+    zc = db.Column(db.Integer, unique=False, nullable=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.user_name
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "wishfearLESS" : self.wishfearLESS,
+            "previous_help" : self.previous_help,
+            "zc" : self.zc,
+        }
