116bdbba5cd6acd79823863748d7bcfed9d82dcc
kvaldesvallejo <kvaldesvallejo@gmail.com>
2021-03-16 01:02:50 +0000

    fix some errors

--- src/main.py
+++ src/main.py
@@ -8,17 +8,17 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User, Therapist , Patient, Testimonial, PatientLesson, Question, Lesson
+from models import db, User, Therapist , Patient, PatientLesson, Lesson
 
-#import cloudinary
-#import cloudinary.uploader
-#import cloudinary.api
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
-#cloudinary.config( 
- # cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME'), 
-  #api_key = os.environ.get('CLOUDINARY_API_KEY'), 
-  #api_secret = os.environ.get('CLOUDINARY_API_SECRET') 
-#)
+cloudinary.config( 
+  cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME'), 
+  api_key = os.environ.get('CLOUDINARY_API_KEY'), 
+  api_secret = os.environ.get('CLOUDINARY_API_SECRET') 
+)
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -39,75 +39,85 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# @app.route('/user', methods=['GET'])
+# def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+#     response_body = {
+#         "msg": "Hello, this is your GET /user response "
+#     }
 
-    return jsonify(response_body), 200
+#     return jsonify(response_body), 200
 
 #-----------------USER-----------------------------------------------------------
 # Create a new user 
 @app.route('/user', methods=['POST'])
 def create_user():
     body = request.get_json()
+    #@profile_picture = request.files["profile_picture"]
     
-    if body is None:
-        raise APIException("You need to specify the request body as a json object", 400)
+    # if body is None:
+    #     raise APIException("You need to specify the request body as a json object", 400)
 
-    # Validations
-    #First Name
-    if "fisrt_name" in body:
-        fn = body['fisrt_name']
-    else:
-        raise APIException("You need to specify the first name", 400)
-    #Last name
-    if "last_name" in body:
-        ln = body['last_name']
-    else:
-        raise APIException("You need to specify an Last Name", 400)
-    #email
-    if "email" in body:
-        e= body["email"]
-    else:
-        raise APIException("You need to specify an email", 400)
-    if User.query.filter_by(email=body['email']).first() is not None:
-        raise APIException("email is in use", 400)
-    #phone_number
-    if "phone_number" in body:
-        phn = body["phone_number"]
-    else:
-        raise APIException("You need to specify an phone_number", 400)
-    if User.query.filter_by(phone_number=body['phone_number']).first() is not None:
-        raise APIException("phone_number is in use", 400)
-    #user_name
-    if "user_name" in body:
-        un = body["user_name"]
-    else:
-        raise APIException("You need to specify an user_name", 400)
-    if User.query.filter_by(user_name=body['user_name']).first() is not None:
-        raise APIException("user name is in use", 400)
-    #password
-    if "password" in body:
-        p = body["password"]
-    else:
-        raise APIException("You need to specify an password", 400)
-    if User.query.filter_by(password=body['password']).first() is not None:
-        raise APIException("password is in use", 400)
+    # # Validations
+    # #First Name
+    # if "first_name" in body:
+    fn = body['first_name']
+    # else:
+    #     raise APIException("You need to specify the first name", 400)
+    # #Last name
+    # if "last_name" in body:
+    ln = body['last_name']
+    # else:
+    #     raise APIException("You need to specify an Last Name", 400)
+    # #email
+    # if "email" in body:
+    e= body["email"]
+    # else:
+    #     raise APIException("You need to specify an email", 400)
+    # if User.query.filter_by(email=body['email']).first() is not None:
+    #     raise APIException("email is in use", 400)
+    # #phone_number
+    # if "phone_number" in body:
+    phn = body["phone_number"]
+    # else:
+    #     raise APIException("You need to specify an phone_number", 400)
+    # if User.query.filter_by(phone_number=body['phone_number']).first() is not None:
+    #     raise APIException("phone_number is in use", 400)
+    # #user_name
+    # if "user_name" in body:
+    un = body["user_name"]
+    # else:
+    #     raise APIException("You need to specify an user_name", 400)
+    # if User.query.filter_by(user_name=body['user_name']).first() is not None:
+    #     raise APIException("user name is in use", 400)
+    # #password
+    # if "password" in body:
+    p = body["password"]
+    # else:
+    #     raise APIException("You need to specify an password", 400)
+    
     #profile_picture
-    if "profile_picture" in body:
-        pp = body["profile_picture"]
-    else:
-        pp = "default"
-    #account_type
-    if "account_type" in body:
-        at = body["account_type"]
-    else:
-        raise APIException("You need to specify the account_type", 400)
+    #if "profile_picture" is not None:
+        #pp = request.files["profile_picture"]
+        # pp = "---------------"
+        # print("---------------------------------------------------")
+        #print(profile_picture)
+        #picture_uploaded = cloudinary.uploader.upload(profile_picture)
+            # options = {
+            #            “use_filename”: True  # use filename as public id on cloudinary
+            #         })
+        #pp = picture_uploaded['secure_url']
+    # else:
+    #     print("************************************************")
+    pp = "default"
 
-    new_user = User(fisrt_name = fn, 
+    # #account_type
+    # if "account_type" in body:
+    at = body["account_type"]
+    # else:
+    #     at="0"
+
+    new_user = User(first_name = fn, 
                     last_name = ln, 
                     email= e, 
                     phone_number = phn, 
@@ -121,17 +131,19 @@ def create_user():
     db.session.add(new_user)
     db.session.commit()
 
-    users = User.query.all()
-    response_body = list(map(lambda x: x.serialize(), users))
-    return jsonify(response_body), 200
+    user = User.query.filter_by(email=e).first()
+    print(user)
+
+    #response_body = list(map(lambda x: x.serialize(), user))
+    return jsonify(user.serialize()), 200
 
 # Get all users
-@app.route('/users', methods=['GET'])
+@app.route('/user', methods=['GET'])
 def get_users():
 
     users = User.query.all()
     response_body = list(map(lambda x: x.serialize(), users))
-    print(response_body)
+    #print(response_body)
     print(users)
     return jsonify(response_body), 200    
 
@@ -159,8 +171,8 @@ def handle_single_user(user_id):
 
     # Validations
     #First Name
-    if "fisrt_name" in body:
-        target_user.fisrt_name = body['fisrt_name']
+    if "first_name" in body:
+        target_user.first_name = body['first_name']
     
     #Last name
     if "last_name" in body:
@@ -190,7 +202,18 @@ def handle_single_user(user_id):
     
     #profile_picture
     if "profile_picture" in body:
-        target_user.profile_picture = body["profile_picture"]
+        
+        target_user.profile_picture = request.FILES["profile_picture"]
+        print("_______________________")
+        print(request.files["profile_picture"])
+        picture_uploaded = cloudinary.uploader.upload(target_user.profile_picture)
+       
+            #options = {
+                #        “use_filename”: True  # use filename as public id on cloudinary
+                #    })
+        target_user.profile_picture = picture_uploaded['secure_url']
+    else:
+        print("******************************************")
 
     #account_type
     if "account_type" in body:
@@ -248,7 +271,7 @@ def create_therapist():
     return jsonify(response_body), 200
        
 # Get all Therapist
-@app.route('/therapists', methods=['GET'])
+@app.route('/therapist', methods=['GET'])
 def get_therapists():
     therapists = Therapist.query.all()
     response_body = list(map(lambda x: x.serialize(), therapists))
@@ -300,7 +323,7 @@ def update_therapist(user_id):
     response_body = list(map(lambda x: x.serialize(), therapists))
     return jsonify(response_body), 200
 
-#-----------------Patient-----------------------------------------------------------
+#-------------------------Patient---------------------------------------------------
 # Create a new Patient 
 @app.route('/patient', methods=['POST'])
 def create_patient():
@@ -316,22 +339,24 @@ def create_patient():
         ui = body["user_id"]
     else:
         raise APIException("You need to specify an user_id", 400)
-    #wishfearLESS
-    if "wishfearLESS" in body:
-        w = body["wishfearLESS"]
+    #phobia
+    if "phobia" in body:
+        p = body["phobia"]
+    #wishfearless
+    if "wishfearless" in body:
+        w = body["wishfearless"]
     #previous_help
     if "previous_help" in body:
         ph = body["previous_help"]
-    #zc
-    if "zc" in body:
-        zipc = body["zc"]
-    else:
-        raise APIException("You need to specify the languages_spoke", 400)
+    #severity
+    if "severity" in body:
+        s = body["severity"]
 
     new_patient = Patient(user_id = ui, 
-                    wishfearLESS = w, 
+                    phobia = p,
+                    wishfearless = w, 
                     previous_help = ph, 
-                    zc = zipc)
+                    severity = s)
 
     db.session.add(new_patient)
     db.session.commit()
@@ -341,7 +366,7 @@ def create_patient():
     return jsonify(response_body), 200
 
 # Get all Patient
-@app.route('/patients', methods=['GET'])
+@app.route('/patient', methods=['GET'])
 def get_patients():
     patients = Patient.query.all()
     response_body = list(map(lambda x: x.serialize(), patients))
@@ -374,23 +399,241 @@ def update_patient(user_id):
     #user_id
     if "user_id" in body:
         target_patient.user_id = body['user_id']
+
+    #phobia
+    if "phobia" in body:
+       target_patient.phobia = body['phobia']
     
-    #wishfearLESS
-    if "wishfearLESS" in body:
-       target_patient.wishfearLESS = body['wishfearLESS']
+    #wishfearless
+    if "wishfearless" in body:
+       target_patient.wishfearless = body['wishfearless']
     
     #previous_help
     if "previous_help" in body:
         target_patient.previous_help = body['previous_help']
    
-    #zc
-    if "zc" in body:
-        target_patient.zc = body['zc']
+    #severity
+    if "severity" in body:
+        target_patient.severity = body['severity']
 
     db.session.commit()
 
     patients = Patient.query.all()
     response_body = list(map(lambda x: x.serialize(), patients))
+    return jsonify(response_body), 200
+
+#--------------------------Lesson---------------------------------------------------------
+# Get all Lesson
+@app.route('/lesson', methods=['GET'])
+def get_lesons():
+    lessons = Lesson.query.all()
+    response_body = list(map(lambda x: x.serialize(), lessons))
+    return jsonify(response_body), 200
+
+# Create a new Lesson 
+@app.route('/lesson', methods=['POST'])
+def create_lesson():
+
+    body = request.get_json()
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", 400)
+
+    # Validations
+    #name
+    if "name" in body:
+        n = body["name"]
+    else:
+        raise APIException("You need to specify the name", 400)
+    #phobia_description
+    if "phobia_description" in body:
+        pd = body["phobia_description"]
+    else:
+        pd = ""
+    #description_1
+    if "description_1" in body:
+        d1 = body["description_1"]
+    else:
+        d1 = ""
+    #img_2
+    if "img_2" in body:
+        i2 = body["img_2"]
+    else:
+        i2 = ""
+    #description_2
+    if "description_2" in body:
+        d2 = body["description_2"]
+    else:
+        d2 = ""
+    #description_3
+    if "description_3" in body:
+        d3 = body["description_3"]
+    else:
+        d3 = ""
+    #search_3
+    if "search_3" in body:
+        s3 = body["search_3"]
+    else:
+        s3 = ""
+    #img_4
+    if "img_4" in body:
+        i4 = body["img_4"]
+    else:
+        i4 = ""
+    #description_4
+    if "description_4" in body:
+        d4 = body["description_4"]
+    else:
+        d4 = ""
+    #img_5
+    if "img_5" in body:
+        i5 = body["img_5"]
+    else:
+        i5 = ""
+    #description_5
+    if "description_5" in body:
+        d5 = body["description_5"]
+    else:
+        d5 = ""
+    #search_5
+    if "search_5" in body:
+        s5 = body["search_5"]
+    else:
+        s5 = ""
+    #description_6
+    if "description_6" in body:
+        d6 = body["description_6"]
+    else:
+        d6 = ""
+        #raise APIException("You need to specify the description 6", 400)
+    
+    new_lesson = Lesson(name = n, 
+                    phobia_description = pd, 
+                    description_1 = d1, 
+                    img_2 = i2,
+                    description_2 = d2,
+                    description_3 = d3,
+                    search_3 = s3,
+                    img_4 = i4,
+                    description_4 = d4,
+                    img_5 = i5,
+                    description_5 = d5,
+                    search_5 = s5, 
+                    description_6 = d6,
+                    )
+
+    db.session.add(new_lesson)
+    db.session.commit()
+
+    lessons = Lesson.query.all()
+    response_body = list(map(lambda x: x.serialize(), lessons))
+    return jsonify(response_body), 200
+
+#-------------------------PatientLesson-------------------------------------------------
+# Get all PatientLesson
+@app.route('/patientlesson', methods=['GET'])
+def get_patient_lesson():
+    patientlessons = PatientLesson.query.all()
+    response_body = list(map(lambda x: x.serialize(), patientlessons))
+    return jsonify(response_body), 200
+
+# Create a new Lesson 
+@app.route('/patientlesson', methods=['POST'])
+def create_patient_lesson():
+
+    body = request.get_json()
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", 400)
+
+    # Validations
+    #id_lesson
+    if "id_lesson" in body:
+        il = body["id_lesson"]
+    else:
+        raise APIException("You need to specify the id_lesson", 400)
+    #id_patient
+    if "id_patient" in body:
+        ip = body["id_patient"]
+    else:
+        raise APIException("You need to specify the id_patient", 400)
+    #question_2_feeling
+    if "question_2_feeling" in body:
+        q2f = body["question_2_feeling"]
+    else:
+        q2f = ""
+    #question_2_experience
+    if "question_2_experience" in body:
+        q2e = body["question_2_experience"]
+    else:
+        q2e = ""
+    #question_2_date
+    if "question_2_date" in body:
+        q2d = body["question_2_date"]
+    else:
+        q2d = ""
+
+    #question_4_feeling
+    if "question_4_feeling" in body:
+        q4f = body["question_4_feeling"]
+    else:
+        q4f = ""
+    #question_4_experience
+    if "question_4_experience" in body:
+        q4e = body["question_4_experience"]
+    else:
+        q4e = ""
+    #question_4_date
+    if "question_4_date" in body:
+        q4d = body["question_4_date"]
+    else:
+        q4d = ""
+    #description_4
+    if "description_4" in body:
+        d4 = body["description_4"]
+    else:
+        d4 = ""
+
+    #question_5_feeling
+    if "question_5_feeling" in body:
+        q5f = body["question_5_feeling"]
+    else:
+        q5f = ""
+    #question_5_experience
+    if "question_5_experience" in body:
+        q5e = body["question_5_experience"]
+    else:
+        q5e = ""
+    #question_5_date
+    if "question_5_date" in body:
+        q5d = body["question_5_date"]
+    else:
+        q5d = ""
+       
+    #actual_step
+    if "actual_step" in body:
+        ast = body["actual_step"]
+    else:
+        ast = 0
+    
+    new_patient_lesson = PatientLesson(name = n, 
+                    id_lesson = il, 
+                    id_patient = ip, 
+                    question_2_feeling = q2f,
+                    question_2_experience = q2e,
+                    question_2_date = q2d,
+                    question_4_feeling = q4f,
+                    question_4_experience = q4e,
+                    question_4_date = q4d,
+                    question_5_feeling = q5f,
+                    question_5_experience = q5e,
+                    question_5_date = q5d, 
+                    actual_step = ast,
+                    )
+
+    db.session.add(new_patient_lesson)
+    db.session.commit()
+
+    new_patient_lessons = PatientLesson.query.all()
+    response_body = list(map(lambda x: x.serialize(), new_patient_lessons))
     return jsonify(response_body), 200
 
 # this only runs if `$ python src/main.py` is executed
--- src/models.py
+++ src/models.py
@@ -4,18 +4,22 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    fisrt_name = db.Column(db.String(10), unique=False, nullable=False)
-    last_name = db.Column(db.String(20), unique=False, nullable=False)
-    phone_number = db.Column(db.String(15), unique=True, nullable=False)
-    user_name = db.Column(db.String(20), unique=True, nullable=False)
-    profile_picture = db.Column(db.String(120), nullable=True)
-    account_type = db.Column(db.Integer, unique=False, nullable=False) 
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    first_name = db.Column(db.String(300), unique=False, nullable=False)
+    last_name = db.Column(db.String(300), unique=False, nullable=False)
+    phone_number = db.Column(db.String(300), unique=True, nullable=False)
+    user_name = db.Column(db.String(300), unique=True, nullable=False)
+    profile_picture = db.Column(db.String(300), nullable=True)
+     
+    email = db.Column(db.String(300), unique=True, nullable=False)
+    password = db.Column(db.String(300), unique=False, nullable=False)
+
+    testimonial_desciption = db.Column(db.String(300), unique=False, nullable=True)
+    testimonial_photo = db.Column(db.String(500), unique=False, nullable=True)
+    testimonial_date = db.Column(db.String(15), unique=False, nullable=True)
+
 
     user_therapist = db.relationship("Therapist", uselist=False, backref="user")
     user_patient = db.relationship("Patient", uselist=False, backref="user")
-    testimonial = db.relationship("Testimonial", uselist=False, backref="user")
 
     def __repr__(self):
         return '<User %r>' % self.user_name
@@ -23,12 +27,16 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "fisrt_name" : self.fisrt_name,
+            "first_name" : self.first_name,
             "last_name" : self.last_name,
             "phone_number" : self.phone_number,
             "user_name" : self.user_name,
             "account_type" : self.account_type, 
             "email": self.email,
+            "testimonial_desciption": self.testimonial_desciption,
+            "testimonial_date": self.testimonial_date,
+            "profile_picture": self.profile_picture
+
             #"user_therapist": self.user_therapist.serialize() if self.therapist else None,
             #"user_patient": self.user_patient.serialize() if not self.therapist else None,
             # do not serialize the password, its a security breach
@@ -36,7 +44,7 @@ class User(db.Model):
 
 class Therapist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), unique=True)
     ofice_location = db.Column(db.String(30), unique=False, nullable=False)
     experience = db.Column(db.String(500), unique=False, nullable=False)
     languages_spoke = db.Column(db.String(300), unique=False, nullable=False)
@@ -55,10 +63,12 @@ class Therapist(db.Model):
 
 class Patient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    wishfearLESS = db.Column(db.String(300), unique=False, nullable=True)
-    previous_help = db.Column(db.Boolean, unique=False, nullable=True)
-    zc = db.Column(db.Integer, unique=False, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), unique=True)
+    phobia = db.Column(db.String(300), unique=False, nullable=True)
+    wishfearless = db.Column(db.String(300), unique=False, nullable=True)
+    previous_help = db.Column(db.String(300), unique=False, nullable=True)
+    severity = db.Column(db.String(300), unique=False, nullable=True)
+
 
     patient_lesson = db.relationship("PatientLesson", uselist=False, backref="patient")
 
@@ -69,36 +79,29 @@ class Patient(db.Model):
         return {
             "id" : self.id,
             "user_id": self.user_id,
-            "wishfearLESS" : self.wishfearLESS,
+            "phobia": self.phobia,
+            "wishfearless" : self.wishfearless,
             "previous_help" : self.previous_help,
-            "zc" : self.zc,
-        }
-
-class Testimonial(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    desciption = db.Column(db.String(300), unique=False, nullable=True)
-    testimonial_photo = db.Column(db.String(500), unique=False, nullable=True)
-    date = db.Column(db.String(15), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<Testimonial %r>' % self.id
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "user_id" : self.user_id,
-            "desciption" : self.desciption,
-            "date" : self.date
+            "severity" : self.severity
         }
 
 class PatientLesson(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     id_lesson = db.Column(db.Integer, db.ForeignKey("lesson.id"), nullable=False)
     id_patient = db.Column(db.Integer, db.ForeignKey("patient.id"))
-    question_2 = db.Column(db.Integer, db.ForeignKey("question.id"))
-    #question_4 = db.Column(db.Integer, db.ForeignKey("question.id"))
-    #question_5 = db.Column(db.Integer, db.ForeignKey("question.id"))
+
+    question_2_feeling = db.Column(db.String(300), unique=False, nullable=True)
+    question_2_experience = db.Column(db.String(300), unique=False, nullable=True)
+    question_2_date = db.Column(db.String(300), unique=False, nullable=True)
+
+    question_4_feeling = db.Column(db.String(300), unique=False, nullable=True)
+    question_4_experience = db.Column(db.String(300), unique=False, nullable=True)
+    question_4_date = db.Column(db.String(300), unique=False, nullable=True)
+
+    question_5_feeling = db.Column(db.String(300), unique=False, nullable=True)
+    question_5_experience = db.Column(db.String(300), unique=False, nullable=True)
+    question_5_date = db.Column(db.String(300), unique=False, nullable=True)
+
     actual_step = db.Column(db.Integer, unique=False, nullable=False)
 
     def __repr__(self):
@@ -109,50 +112,29 @@ class PatientLesson(db.Model):
             "id": self.os,
             "id_lesson": self.id_lesson,
             "id_patient" : self.id_patient,
-            "question_2" : self.question_2,
-            #"question_4" : self.question_4,
-            #"question_5" : self.question_5,
             "actual_step" : self.actual_step
-        }
-
-class Question(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    feeling = db.Column(db.String(300), unique=False, nullable=True)
-    experience = db.Column(db.String(300), unique=False, nullable=True)
-    step = db.Column(db.Integer, unique=False, nullable=False)
-    date = db.Column(db.String(15), unique=False, nullable=False)
-
-    patient_phobia = db.relationship("PatientLesson", uselist=False, backref="question")
-
-    def __repr__(self):
-        return '<Question %r>' % self.id
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "feeling" : self.feeling,
-            "experience" : self.experience,
-            "step" : self.step,
-            "date" : self.date
         }
 
 class Lesson(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(10), unique=False, nullable=False)
+    name = db.Column(db.String(300), unique=True, nullable=False)
     phobia_description = db.Column(db.String(500), unique=False, nullable=False)
+
     description_1 = db.Column(db.String(500), unique=False, nullable=False)
-    img_2 = db.Column(db.String(500), unique=False, nullable=False)
+
+    img_2 = db.Column(db.String(500), unique=False, nullable=True)
     description_2 = db.Column(db.String(500), unique=False, nullable=False)
-    question_2 = db.Column(db.Integer, unique=False, nullable=False)
+    
     description_3 = db.Column(db.String(500), unique=False, nullable=False)
     search_3 = db.Column(db.String(500), unique=False, nullable=False)
-    img_4 = db.Column(db.String(500), unique=False, nullable=False)
+
+    img_4 = db.Column(db.String(500), unique=False, nullable=True)
     description_4 = db.Column(db.String(500), unique=False, nullable=False)
-    question_4 = db.Column(db.Integer, unique=False, nullable=False)
-    img_5 = db.Column(db.String(500), unique=False, nullable=False)
+    
+    img_5 = db.Column(db.String(500), unique=False, nullable=True)
     description_5 = db.Column(db.String(500), unique=False, nullable=False)
-    question_5 = db.Column(db.Integer, unique=False, nullable=False)
     search_5 = db.Column(db.String(500), unique=False, nullable=False)
+
     description_6 = db.Column(db.String(500), unique=False, nullable=False)
 
     patient_phobia = db.relationship("PatientLesson", backref="lesson", uselist=False) 
@@ -168,15 +150,12 @@ class Lesson(db.Model):
             "description_1" : self.description_1,
             "img_2" : self.img_2,
             "description_2" : self.description_2, 
-            "question_2": self.question_2,
             "description_3": self.description_3,
             "search_3": self.search_3,
             "img_4": self.img_4,
             "description_4": self.description_4,
-            "question_4": self.question_4,
             "img_5": self.img_5,
             "description_5": self.description_5,
-            "question_5": self.question_5,
             "search_5": self.search_5,
             "description_6": self.description_6,
 
